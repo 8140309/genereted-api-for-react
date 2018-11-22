2bd4315eef494bc4467bb5338d70af077edf426f
Fernando Sousa <8140309@estg.ipp.pt>
2018-11-22 18:55:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var config = require('./config.js');
 
  app.use(bodyParser.urlencoded({ extended: true }));
  app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
  var usersModel = require('./api/models/usersModel');
  var usersRoute = require('./api/routes/usersRoute');
